272a4e8a5fff59fb433f3b4be9ab962b7a74423b
isanch14 <isanch14@stu.southtexascollege.edu>
2020-03-07 13:42:17 -0600

    Stuff

---
+++ src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="flex items-center justify-between flex-wrap bg-purple-900 p-6">
+    <router-link to="/" class="flex items-center flex-shrink-0 text-white mr-6">
+      <img class="h-8 mr-4" src="superhero.png" alt="Logo" />
+      <span class="font-semibold text-xl tracking-tight"
+        >Search<span class="font-light">SuperHero</span></span
+      >
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+}
+</script>
